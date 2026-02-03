7d5f326787088724e9f4d03a99ab9e0634959873
kyunghyun306 <rud03gus06@naver.com>
2026-02-03 21:37:25 +0900

    스크립트에 변수들 네이밍 컨벤션 규칙 반영하여 수정

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,27 +8,27 @@ public class PlayerMove : MonoBehaviour
     public Joystick joystick;  // 조이스틱 연결
     public float move_speed = 3f;  // 플레이어 이동 속도
 
-    private Rigidbody2D rb;  // 물리 엔진을 사용하기 위한 컴포넌트
-    private Vector2 move_vector;  // 조이스틱 입력값을 담을 벡터
+    private Rigidbody2D _rb;  // 물리 엔진을 사용하기 위한 컴포넌트
+    private Vector2 _move_vector;  // 조이스틱 입력값을 담을 벡터
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         // 조이스틱에서 x축과 y축의 입력값 받아오기
-        move_vector.x = joystick.Horizontal;
-        move_vector.y = joystick.Vertical;
+        _move_vector.x = joystick.Horizontal;
+        _move_vector.y = joystick.Vertical;
     }
 
     void FixedUpdate()
     {
         // 물리 엔진을 이용해 플레이어 위치 이동
         // 현재 위치 + (방향 * 속도 * 시간 보정)
-        Vector2 current_pos = rb.position;
-        Vector2 move_amount = move_vector * move_speed * Time.fixedDeltaTime;
-        rb.MovePosition(current_pos + move_amount);
+        Vector2 current_pos = _rb.position;
+        Vector2 move_amount = _move_vector * move_speed * Time.fixedDeltaTime;
+        _rb.MovePosition(current_pos + move_amount);
     }
 }
