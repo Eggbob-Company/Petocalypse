5ad50fc913785a23f78d1a9b76cf285f351c235c
nieah78 <nieah@hanyang.ac.kr>
2026-02-17 20:53:11 +0900

    경험치 프리팹 및 기본 스크립트 생성

---
+++ Assets/Scripts/ExpSpawn.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ExpSpawn : PoolAble // 오브젝트 풀 사용을 위한 PoolAble 상속
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
